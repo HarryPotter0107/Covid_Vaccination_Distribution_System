8b3fc955c607102b35a5dd5c7e4e9e1f3844cca4
HarryPotter0107 <bhanuupadhyay87@gmail.com>
2023-12-19 14:34:29 +0530

    registe dpctor functionality got created

--- src/main/java/com/covidvaccination/distributionsystem/Covid/Vaccination/Distribution/Backend/repository/VaccinationCenterRepository.java
+++ src/main/java/com/covidvaccination/distributionsystem/Covid/Vaccination/Distribution/Backend/repository/VaccinationCenterRepository.java
@@ -1,7 +1,9 @@
 package com.covidvaccination.distributionsystem.Covid.Vaccination.Distribution.Backend.repository;
 
 import com.covidvaccination.distributionsystem.Covid.Vaccination.Distribution.Backend.models.VaccinationCenter;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,9 @@ import java.util.UUID;
 public interface VaccinationCenterRepository extends JpaRepository<VaccinationCenter, UUID> {
     @Query(value = "select * from vaccination_center where doctor_count =(select min(doctor_count) from vaccination_center)",nativeQuery = true)
     public List<VaccinationCenter> getMinimumDoctorVaccinationCenter();
+
+    @Modifying
+    @Transactional
+    @Query(value = "update vaccination_center set doctor_count =:docCount where id =:id", nativeQuery = true)
+    public void updateDocCountByOne(UUID id, int docCount);
 }
